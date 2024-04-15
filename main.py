7b1fa7b9610bdf32cfb9ab75ac7714ad2544bff7
c_pzzo <cesar.pozo@infosapiens.mx>
2024-04-14 18:21:09 -0600

    Added "home.html" file. Updated main.py to redirect to "home.html".

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # When the url "/" is accessed, then return the string.
 @app.route("/")
 def greeting():
-    return "<p>My first Flask project!</p>"
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug = True)
